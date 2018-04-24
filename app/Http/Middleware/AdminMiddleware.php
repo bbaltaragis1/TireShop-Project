6ab17721964a6eb20e12f650079e845711ad8aef
Adamo <heymo1@aol.com>
2018-04-24 15:55:32 -0500

    fixed registration

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+public function handle($request, Closure $next)
+    {
+
+        if(Auth::user()->isAdmin == '1') // is an admin
+        {
+            return $next($request); // pass the admin
+        }
+
+        return redirect('/home'); // not admin. redirect whereever you like
+
+    }}
